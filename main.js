04693adc4266342b13f37af43fb40740a617c72f
Nathan Manske <nathan.manske@utexas.edu>
2017-08-23 22:07:39 -0500

    Add json read and append; simple randomization element

--- main.js
+++ main.js
@@ -1,14 +1,41 @@
 $(document).ready(function() {
+
+  function shuffle(a) {
+    for (let i = a.length; i; i--) {
+      let j = Math.floor(Math.random() * i);
+      [a[i - 1], a[j]] = [a[j], a[i - 1]];
+    }
+  }
+
   function displayFact(fact) {
     $("#fact").text(fact);
   }
+
   $("#health-btn").click(function() {
-    displayFact("Plants are good for you!")
+    var factBox = $('#factBox');
+
+    $.getJSON('./static/data/data.json', function(data) {
+      var items = data.health.map(function(item) {
+        return item.id + ': ' + item.value;
+      });
+
+      shuffle(items);
+      factBox.empty();
+
+      if (items.length) {
+        var content = '<li>' + items.join('</li><li>') + '</li>';
+        var list = $('<ul />').html(content);
+        factBox.append(list);
+      }
+    });
   });
+
   $("#environment-btn").click(function() {
-    displayFact("Save the trees!")
+
   });
+
   $("#ethics-btn").click(function() {
-    displayFact("Animals are cute!")
+
   });
+
 });
