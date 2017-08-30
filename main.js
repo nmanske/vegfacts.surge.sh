3291eb540aeecec0db73a89b805dcb8b7591eced
Nathan Manske <nathan.manske@utexas.edu>
2017-08-29 22:33:36 -0500

    Make factDisplay() prettier and add spacing

--- main.js
+++ main.js
@@ -3,11 +3,13 @@ $(document).ready(function() {
   function displayFact(type) {
     $.getJSON('./static/data/facts.json', function(data) {
 
-      let fact = (type == 'Health') ? data.health[Math.floor(Math.random() * data.health.length)] :
-        (type == 'Environment') ? data.environment[Math.floor(Math.random() * data.environment.length)] :
-        data.ethics[Math.floor(Math.random() * data.ethics.length)]
+      let category = (type == 'Health') ? data.health :
+        (type == 'Environment') ? data.environment :
+        data.ethics
 
-      let content = fact.description;
+      let fact = category[Math.floor(Math.random() * category.length)]
+
+      let content = '<br>' + fact.description + '<br>';
       for (let i = 0; i < fact.source.length; i++)
         content += '<br>Source: <a href="' + fact.source[i].url + '">' + fact.source[i].name + '</a>'
 
