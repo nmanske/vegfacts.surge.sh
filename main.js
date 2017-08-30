467e62be8b25b84492572fd05fae0452011893b2
Nathan Manske <nathan.manske@utexas.edu>
2017-08-29 22:27:21 -0500

    Display multiple sources at once

--- main.js
+++ main.js
@@ -2,36 +2,37 @@ $(document).ready(function() {
 
   function getRandomElement(a) {
     for (let i = a.length; i; i--) {
-      let j = Math.floor(Math.random() * i);
-      [a[i - 1], a[j]] = [a[j], a[i - 1]];
+      let j = Math.floor(Math.random() * i)[a[i - 1], a[j]] = [a[j], a[i - 1]]
     }
   }
 
   function displayFact(type) {
-
     $.getJSON('./static/data/facts.json', function(data) {
-      let chosen = data[Math.floor(Math.random()*data.length)];
 
-      let fact = (type == 'Health') ? data.health[Math.floor(Math.random()*data.health.length)] :
-        (type == 'Environment') ? data.environment[Math.floor(Math.random()*data.environment.length)] :
-        data.ethics[Math.floor(Math.random()*data.ethics.length)];
+      let fact = (type == 'Health') ? data.health[Math.floor(Math.random() * data.health.length)] :
+        (type == 'Environment') ? data.environment[Math.floor(Math.random() * data.environment.length)] :
+        data.ethics[Math.floor(Math.random() * data.ethics.length)]
 
-      let content = fact.description + '<br>Source: <a href="' + fact.url + '">' + fact.source + '</a>';
-      $('#factBox').empty();
-      $('#factBox').append(content);
-    });
+      let content = fact.description;
+      for (let i = 0; i < fact.source.length; i++)
+        content += '<br>Source: <a href="' + fact.source[i].url + '">' + fact.source[i].name + '</a>'
+
+      $('#factBox').empty()
+      $('#factBox').append(content)
+
+    })
   }
 
   $('#health-btn').click(function() {
-    displayFact('Health');
-  });
+    displayFact('Health')
+  })
 
   $('#environment-btn').click(function() {
-    displayFact('Environment');
-  });
+    displayFact('Environment')
+  })
 
   $('#ethics-btn').click(function() {
-    displayFact('Ethics');
-  });
+    displayFact('Ethics')
+  })
 
-});
+})
